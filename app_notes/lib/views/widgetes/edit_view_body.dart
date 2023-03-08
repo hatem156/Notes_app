ed750d952e978c35c8e04a2348fbc5e845dd0158
hatem156 <126658469+hatem156@users.noreply.github.com>
2023-03-08 17:16:22 +0200

    creat tiltle field in edit note view

--- app_notes/lib/views/widgetes/edit_view_body.dart
+++ app_notes/lib/views/widgetes/edit_view_body.dart
@@ -1,3 +1,6 @@
+import 'package:app_notes/views/widgetes/custom_app_bar.dart';
+import 'package:app_notes/views/widgetes/custom_text_field.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -8,6 +11,21 @@ class Editnoteviewbody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const[
+          SizedBox(height: 50,),
+          Customappbar(
+            title: 'Edit note',
+            icon: Icons.check,
+          ),
+          SizedBox(height: 50,),
+          Customtextfield(hint:'Title'),
+          SizedBox(height: 15,),
+          Customtextfield(hint:'content'),
+          ],
+      ),
+    );
   }
 }
