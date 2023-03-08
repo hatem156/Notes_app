55334ba1241f9d3054db0481cee56db527a2d787
hatem156 <126658469+hatem156@users.noreply.github.com>
2023-03-08 21:42:32 +0200

    take input

--- app_notes/lib/views/widgetes/cusstom_button.dart
+++ app_notes/lib/views/widgetes/cusstom_button.dart
@@ -2,25 +2,29 @@ import 'package:flutter/material.dart';
 
 import 'constant.dart';
 class Custombutton extends StatelessWidget {
-  const Custombutton({super.key});
+  const Custombutton({super.key, this.ontap});
 
+final void Function()? ontap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: kprimarycolor, 
-        borderRadius: BorderRadius.circular(8)
-      ),
-      child:const Center(
-        child:Text('Add', 
-        style:TextStyle(
-          color: Colors.black,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),),
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          color: kprimarycolor, 
+          borderRadius: BorderRadius.circular(8)
         ),
+        child:const Center(
+          child:Text('Add', 
+          style:TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),),
+          ),
+      ),
     );
   }
 }
--- app_notes/lib/views/widgetes/custom_text_field.dart
+++ app_notes/lib/views/widgetes/custom_text_field.dart
@@ -2,14 +2,23 @@ import 'package:app_notes/views/widgetes/constant.dart';
 import 'package:flutter/material.dart';
 
 class Customtextfield extends StatelessWidget {
-  const Customtextfield({super.key, required this.hint, this.maxlines=1 });
+  const Customtextfield({super.key, required this.hint, this.maxlines=1, this.onsaved });
 
 final String hint ;
 final int maxlines ;
+final void Function(String?)? onsaved;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onsaved,
+      validator: (value) {
+        if(value?.isEmpty ?? true){
+          return 'field is required';
+        }else{
+          return null;
+        }
+      },
       cursorColor: kprimarycolor,
       maxLines: maxlines,
       decoration: InputDecoration(
